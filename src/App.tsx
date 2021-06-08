0d058496a3b3a6406d8b233d2ea3a9b53975bbe4
wangnanping <1341540464@qq.com>
2021-06-08 21:32:20 +0800

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-// import logo from './assets/images/logo.svg'
-// import robots from './mockdata/robots.json'
-// import Robot from './components/Robot'
-// import styles from './App.module.css'
-// import ShoppingCart from './components/ShoppingCart'
+import React, { useState, useEffect } from 'react'
+import logo from './assets/images/logo.svg'
+import robots from './mockdata/robots.json'
+import Robot from './components/Robot'
+import styles from './App.module.css'
+import ShoppingCart from './components/ShoppingCart'
 
 interface Props {}
 
@@ -11,8 +11,8 @@ interface Props {}
 //   robotGallery: any[]
 // }
 const App: React.FC = prop => {
-  const [count, setCount] = useState(0);
-
+  const [count, setCount] = useState(0)
+  const [robots, getRobots] = useState<any[]>([])
   /**
    * 页面挂载
    */
@@ -22,27 +22,43 @@ const App: React.FC = prop => {
   //     .then(data => this.setState({ robotGallery: data }))
   // }
 
+  useEffect(() => {
+    document.title = `${count}次`
+  }, [count])
+
+  // 传入空数组 相当于生命周期componentDidMount 挂载进页面时候调用一次
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => getRobots(data))
+  }, [])
+
+  // 每一次页面更新都会触发，会造成死循环
+  // useEffect(()=>{
+
+  // })
+
   // render () {
   return (
-    <button
-      onClick={() => {
-        setCount(count + 1)
-      }}
-    >
-      {count}
-    </button>
-    // <div className={styles.app}>
-    //   <div className={styles.appHeader}>
-    //     <img src={logo} className={styles.appLogo} alt='logo' />
-    //     <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
-    //   </div>
-    //   <ShoppingCart />
-    //   <div className={styles.robotList}>
-    //     {robots.map(r => (
-    //       <Robot id={r.id} email={r.email} name={r.name} />
-    //     ))}
-    //   </div>
-    // </div>
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt='logo' />
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+      </div>
+      <button
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        {count}
+      </button>
+      <ShoppingCart />
+      <div className={styles.robotList}>
+        {robots.map(r => (
+          <Robot id={r.id} email={r.email} name={r.name} />
+        ))}
+      </div>
+    </div>
   )
   // }
 }
