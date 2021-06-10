7044a431df145032391298e2221910cd18a334ee
wangnanping <1341540464@qq.com>
2021-06-10 22:00:12 +0800

    useAddToCart自定义HOOK

--- src/components/RobotDiscount.tsx
+++ src/components/RobotDiscount.tsx
@@ -1,30 +1,36 @@
 import React, { useContext } from 'react'
 import Styles from './Robot.module.css'
-import {appContext,appSetStateContext} from "../AppState"
-import {withAddToCart} from "./AddToCart"
+import { appContext, appSetStateContext } from '../AppState'
+import { useAddToCart } from './AddToCart'
 interface RobotProps {
   id: number
   name: string
   email: string
-  addToCart:(id:number,name:string)=>void
 }
-const RobotDiscount: React.FC<RobotProps> = ({ id, name, email,addToCart }) => {
-  const value = useContext(appContext);
+const RobotDiscount: React.FC<RobotProps> = ({ id, name, email }) => {
+  const value = useContext(appContext)
+  const addToCart = useAddToCart()
   return (
     // <appConText.Consumer>
     //   {value => {
     //     return (
-          <li className={Styles.cardContainer}>
-            <img alt='robotes' src={`https://robohash.org/${id}`} />
-            <h2>打折商品</h2>
-            <h2>{name}</h2>
-            <p>{email}</p>
-            <span>作者：{value.userName}</span>
-            <button onClick={()=>{addToCart(id,name)}}>加入购物车</button>
-          </li>
+    <li className={Styles.cardContainer}>
+      <img alt='robotes' src={`https://robohash.org/${id}`} />
+      <h2>打折商品</h2>
+      <h2>{name}</h2>
+      <p>{email}</p>
+      <span>作者：{value.userName}</span>
+      <button
+        onClick={() => {
+          addToCart(id, name)
+        }}
+      >
+        加入购物车
+      </button>
+    </li>
     //     )
     //   }}
     // </appConText.Consumer>
   )
 }
-export default withAddToCart(RobotDiscount);
+export default RobotDiscount
